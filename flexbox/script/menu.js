d2f52d09db93da113af553e7a3a7bd061985a7d5
J.F. Slottje <friso@yourfutureacademy.nl>
2020-07-06 13:44:36 +0200

    flexbox print media query afgemaakt

--- flexbox/script/menu.js
+++ flexbox/script/menu.js
@@ -1,8 +1,8 @@
 /** dit script wordt gebruikt in combinatie met de media query's in het css style sheet om een mobiel vriendelijk menu toe te voegen**/
 let menu_knop = document.querySelector(".menu-knop");
 let menu_items = document.querySelectorAll(".menu-item");
-let secties = document.querySelectorAll("section")
-let breedte = 600;
+let secties = document.querySelectorAll("section");
+let breedte = 900;
 
 /*
  *event listeners
@@ -12,30 +12,28 @@ window.addEventListener("resize", Reset);
 
 /* controleerd of er op een menu knop geklikt is.*/
 menu_knop.addEventListener("click", Toggle);
-menu_items.forEach(function(element) {
+menu_items.forEach(function (element) {
   element.addEventListener("click", Toggle);
 });
 /* controleerd of er op een sectie geklikt is*/
-secties.forEach(function (element){
+secties.forEach(function (element) {
   element.addEventListener("click", Reset);
-})
-
+});
 
 /**
  * De functie toggle veranderd de display style voor de menu items zodat het menu verborgen kan worden achter één menu knop.
  */
 function Toggle() {
-  
   /* controleer of de pagina klein genoeg is voor de menuknop.*/
   if (window.innerWidth < breedte) {
     if (menu_knop.style.display !== "none") {
       menu_knop.style.display = "none";
-      menu_items.forEach(function(element) {
+      menu_items.forEach(function (element) {
         element.style.display = "flex";
       });
     } else {
       menu_knop.style.display = "flex";
-      menu_items.forEach(function(element) {
+      menu_items.forEach(function (element) {
         element.style.display = "none";
       });
     }
@@ -49,12 +47,12 @@ function Reset() {
   /* controleer of de pagina klein genoeg is voor de menuknop.*/
   if (window.innerWidth < breedte) {
     menu_knop.style.display = "flex";
-    menu_items.forEach(function(element) {
+    menu_items.forEach(function (element) {
       element.style.display = "none";
     });
   } else {
     menu_knop.style.display = "none";
-    menu_items.forEach(function(element) {
+    menu_items.forEach(function (element) {
       element.style.display = "flex";
     });
   }
